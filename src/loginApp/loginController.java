2734c571a159116f96b77f354625693f1403ab27
Gurunath Bhat <introvertkernal@users.noreply.github.com>
2017-11-02 10:41:40 +0530

    Databse status added

--- src/loginApp/loginController.java
+++ src/loginApp/loginController.java
@@ -1,4 +1,34 @@
 package loginApp;
 
-public class loginController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class loginController implements Initializable {
+
+    loginModel loginmodel = new loginModel();
+
+    @FXML
+    private TextField username;
+    @FXML
+    private PasswordField password;
+    @FXML
+    private Button loginButton;
+    @FXML
+    Label dbStatus;
+    public void initialize(URL url, ResourceBundle rb){
+      if(this.loginmodel.isDatabaseConnected()){
+          this.dbStatus.setText("Connected");
+        }
+        else{
+          this.dbStatus.setText("Not Connected");
+        }
+
+    }
 }
--- src/loginApp/loginModel.java
+++ src/loginApp/loginModel.java
@@ -1,6 +1,8 @@
 package loginApp;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import dbUtils.dbConnection;
@@ -17,6 +19,35 @@ public class loginModel {
         }
         if(this.connection==null){
             System.exit(1);
+        }
+    }
+
+    public boolean isDatabaseConnected(){
+        return this.connection !=null;
+    }
+    public boolean isLogin(String user,String pass) throws Exception {
+        PreparedStatement pr = null;
+        ResultSet rs = null;
+        String sql = "SELECT * FEOM login where user = ? and pass = ?";
+
+        try {
+            pr = this.connection.prepareStatement(sql);
+            pr.setString(1, user);
+            pr.setString(2, pass);
+            rs = pr.executeQuery();
+            boolean bol1;
+
+            if (rs.next()){
+                return true;
+            }
+            return false;
+        }
+        catch (SQLException ex){
+            return false;
+        }
+        finally {
+            pr.close();
+            rs.close();
         }
     }
 }
